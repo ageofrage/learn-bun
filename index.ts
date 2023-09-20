442b05dc7622ab8159f975d255a43c0b984fe96b
ageofrage <agehara.dev@gmail.com>
2023-09-20 23:57:04 +0900

    :sparkles: init

---
+++ index.ts
@@ -0,0 +1,11 @@
+import figlet from "figlet";
+
+const server = Bun.serve({
+  fetch(req) {
+    const body = figlet.textSync("bun");
+    return new Response(body);
+  },
+  port: 3000,
+});
+
+console.log(`Listening on http://localhost:${server.port} ...`);
